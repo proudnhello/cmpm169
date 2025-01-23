aa8dde91a60f81f082177dd2f84c4a536e8ff41e
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-01-22 19:04:12 -0800

    added experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -2,29 +2,32 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+// Based on Conway's Game of Life, implemented by Wes Modes
+// https://editor.p5js.org/wmodes/sketches/ixuIQcgLi
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const width = 75;
+const height = 75;
+const cellSize = 10;
+let grid;
+let nextGrid;
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+const G_UNDERPOP = 2
+const G_OVERPOP = 3
+const G_REPRO = 3
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+let G_HEALTHY_DIE_SCALE = 0.05
+let G_SPAWN_SCALE = 0.05
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+let g_noiseCounter = 0
+const G_INTERVAL = 0.01
+
+let g_running = true;
+let g_org_die_scale = G_HEALTHY_DIE_SCALE
+let g_org_spawn_scale = G_SPAWN_SCALE
+let g_random = true;
+let g_mouseDown = false  
+let g_lastX = -1
+let g_lastY = -1
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -34,46 +37,145 @@ function resizeScreen() {
   // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
+
 function setup() {
+  createCanvas(width * cellSize, height * cellSize);
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  background(0);
+  grid = create2DArray(width, height);
+  nextGrid = create2DArray(width, height);
+  // randomly populate grid
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      grid[x][y] = floor(random(2));
+    }
+  }
+  
+  console.log("Press p to pause the simulation")
+  console.log("Press c to clear the grid")
+  console.log("Press r to turn off any random effects")
+  console.log("Click and drag to create cells")
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  // if(!(frameCount < 5)){
+  //   return
+  // }
+  // draw current state of grid
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      if (grid[x][y] === 1) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+  
+  if(g_mouseDown){
+    let xIndex = constrain(floor(mouseX/cellSize), 0, width-1)
+    let yIndex = constrain(floor(mouseY/cellSize), 0, height-1)
+    if(xIndex != g_lastX || yIndex != g_lastY){
+      if(grid[xIndex][yIndex] === 0){
+        grid[xIndex][yIndex] = 1
+      }else{
+        grid[xIndex][yIndex] = 0
+      }
+      g_lastX = xIndex
+      g_lastY = yIndex
+    }
+  }
+  
+  if(!g_running){
+    return
+  }
+  
+  let n = (noise(g_noiseCounter) * 2) - 1
+  g_noiseCounter += G_INTERVAL
+  // print(G_POP_CHANCE + n)
+  // update next grid
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let r = random()
+      let neighbors = countNeighbors(x, y);
+      if (grid[x][y] === 1 && (neighbors < G_UNDERPOP || neighbors > G_OVERPOP)) {
+        nextGrid[x][y] = 0;
+      } else if (grid[x][y] === 0 && neighbors === G_REPRO) {
+        nextGrid[x][y] = 1;
+      } else if (grid[x][y] === 1){
+        // Multiplied by - so it happens in the opposite side of the range as spawning
+        if((n*-G_HEALTHY_DIE_SCALE) < r){
+          nextGrid[x][y] = 1;
+        }else{
+          nextGrid[x][y] = 0;
+        }
+      }else{
+        if(G_SPAWN_SCALE * n < r){
+          nextGrid[x][y] = 0;
+        }else{
+          nextGrid[x][y] = 1;
+        }
+      }
+    }
+  }
+  // update grid
+  grid = nextGrid;
+  nextGrid = create2DArray(width, height);
+}
+  
+function keyPressed(){
+  if(key === 'p'){
+    g_running = !g_running
+  }
+  if(key === 'r'){
+    if(g_random){
+      g_random = false
+      G_HEALTHY_DIE_SCALE = 0
+      G_SPAWN_SCALE = 0
+    }else{
+      g_random = true
+      G_HEALTHY_DIE_SCALE = g_org_die_scale
+      G_SPAWN_SCALE = g_org_spawn_scale
+    }
+  }
+  if(key === 'c'){
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        grid[x][y] = 0
+      }
+    }
+  }
+}
+  
+function mousePressed(){
+  g_mouseDown = true
+}
+  
+function mouseReleased(){
+  g_mouseDown = false
+}
+  
+function countNeighbors(x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + width) % width;
+      let row = (y + j + height) % height;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function create2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
 }
