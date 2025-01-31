7269e6a5d095e6611244b9c31e9a1411bccdbd45
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-01-30 21:11:36 -0800

    test

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,29 +1,203 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Moore Macauley
+// Date: 1/30/2025
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+// Based on Filter by angletapes
+// https://openprocessing.org/sketch/1800102
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+var sinOsc, sinFilter, reverb, reverbTime, reverbDecay;
+let bNoise;
+let makingNoise = false;
+let delay;
+let framesPerCircle = 10 
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+let alphaChange = 1/100
+let radiusChange = 1/50
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+let setFreq, occilatorFreq
+
+let drops = []
+
+class drop{
+  constructor(x, y){
+    this.x = x
+    this.y = y
+    this.setFreq = setFreq
+    this.occilatorFreq = occilatorFreq
+    this.currentAlpha = 255
+    this.currentRadius = 0
+  }
+  
+  render(){
+    this.currentAlpha -= this.occilatorFreq * alphaChange
+    this.currentRadius += this.setFreq * radiusChange
+    
+    if(this.currentAlpha <= 0){
+      drops.splice(drops.indexOf(this), 1)
     }
+    
+    stroke(255, 255, 255, this.currentAlpha)
+    circle(this.x, this.y, this.currentRadius)
+  }
+}
 
-    myMethod() {
-        // code to run when method is called
-    }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+  background(0, 0, 0);
+  noFill();
+  stroke(255,255,255)
+  strokeWeight(5)
+
+  print(p5)
+  sinFilter = new p5.Filter();
+  sinFilter.setType("bandpass")
+  console.log(sinFilter)
+
+  //right button oscillator
+  sinOsc = new p5.Oscillator('square');
+  sinOsc.amp(0);
+  sinOsc.freq(200);
+  sinOsc.start();
+  sinOsc.disconnect();
+  sinOsc.connect(sinFilter);
+
+  //reverb for sinOsc
+  reverb = new p5.Reverb();
+  reverb.set(2)
+  reverb.amp(1)
+  sinOsc.connect(reverb)
+  
+  bNoise = new p5.Noise("brown")
+  bNoise.amp(0.5)
+  bNoise.connect()
+  bNoise.start()
+  
+  delay = new p5.Delay()
+  delay.delayTime(0.5)
+  delay.feedback(0.5)
+  sinOsc.connect(delay)
+}
+
+function draw() {
+  background(0,0,0)
+  setFreq = map(mouseX, 0, windowWidth, 50, 500);
+  setFreq = constrain(setFreq, 0, 22050);
+  // let setFreq = (mouseX);
+  sinFilter.freq(setFreq);
+  occilatorFreq = map(mouseY, 0, windowHeight, 50, 500)
+  sinOsc.freq(occilatorFreq)
+  // give the filter a narrow band (lower res = wider bandpass)
+  sinFilter.res(5);//50
+  
+  applyReverb()
+  applyBrown()
+  applyDelay()
+  
+  if(makingNoise){
+    drops.push(new drop(mouseX, mouseY))
+  }
+  
+  drops.forEach((function(drop){
+    drop.render()
+  }))
+}
+
+function applyReverb(){
+  let cornerX = 0
+  let cornerY = 0
+  let centerX = windowWidth/2
+  let centerY = windowHeight/2
+  let maxReverbAmp = 1
+  let distToMouse = Math.sqrt(
+    (mouseX - cornerX) * (mouseX - cornerX) + 
+    (mouseY - cornerY) * (mouseY - cornerY)
+  ) 
+  let distToCenter = Math.sqrt(
+    (cornerX - centerX) * (cornerX - centerX) + 
+    (cornerY - centerY) * (cornerY - centerY)
+  ) 
+  // If we are not in the corner specified, change set and amp to 0
+  if(distToMouse > distToCenter){
+    reverb.amp(0)
+    return
+  }
+  let ratio = distToMouse/distToCenter
+  let rAmp = map(ratio, 1, 0, 0, maxReverbAmp)
+  reverb.amp(rAmp)
+}
+  
+function applyBrown(){
+  let cornerX = 0
+  let cornerY = windowHeight
+  let centerX = windowWidth/2
+  let centerY = windowHeight/2
+  let maxAmp = 0.5
+  let distToMouse = Math.sqrt(
+    (mouseX - cornerX) * (mouseX - cornerX) + 
+    (mouseY - cornerY) * (mouseY - cornerY)
+  ) 
+  let distToCenter = Math.sqrt(
+    (cornerX - centerX) * (cornerX - centerX) + 
+    (cornerY - centerY) * (cornerY - centerY)
+  ) 
+  // If we are not in the corner specified, change set and amp to 0
+  if(distToMouse > distToCenter || !makingNoise){
+    bNoise.amp(0)
+    return
+  }
+  let ratio = distToMouse/distToCenter
+  let rAmp = map(ratio, 1, 0, 0, maxAmp)
+  bNoise.amp(rAmp)
+} 
+  
+function applyDelay(){
+  let cornerX = windowWidth
+  let cornerY = 0
+  let centerX = windowWidth/2
+  let centerY = windowHeight/2
+  let maxAmp = 1
+  let distToMouse = Math.sqrt(
+    (mouseX - cornerX) * (mouseX - cornerX) + 
+    (mouseY - cornerY) * (mouseY - cornerY)
+  ) 
+  let distToCenter = Math.sqrt(
+    (cornerX - centerX) * (cornerX - centerX) + 
+    (cornerY - centerY) * (cornerY - centerY)
+  ) 
+  // If we are not in the corner specified, change set and amp to 0
+  if(distToMouse > distToCenter || !makingNoise){
+    delay.amp(0)
+    return
+  }
+  let ratio = distToMouse/distToCenter
+  let rAmp = map(ratio, 1, 0, 0, maxAmp)
+  delay.amp(rAmp)
+}
+
+function touchStarted(){
+  userStartAudio();
+  sinOsc.amp(0.8);
+  makingNoise = true
+}
+//reverb.decayTime
+function mouseDragged(){
+  userStartAudio();
+  sinOsc.amp(0.8);
+  makingNoise = true
+}
+
+function touchEnded(){
+  sinOsc.amp(0)
+  makingNoise = false
 }
 
 function resizeScreen() {
@@ -32,48 +206,4 @@ function resizeScreen() {
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
 }
