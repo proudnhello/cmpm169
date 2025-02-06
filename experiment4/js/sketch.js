df9ff9c6f19b0304d6f71e006ad93b420f5b1731
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-02-05 22:39:35 -0800

    experminet 5

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,30 +1,6 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+// project.js - purpose and description here
+// Author: Moore Macauley
+// Date: 2/5/2025
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -38,42 +14,158 @@ function resizeScreen() {
 function setup() {
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
 
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+
+  cam = createCamera()
+  cam.ortho()
+  cam.lookAt(0, 0, 0)
+  triangles.push(new Penrose(0,0,0))
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  background(220);
+  
+  triangles.forEach(function(tri){
+    tri.drawPenrose()
+  })
+  
+  // a
+  if(keyIsDown(68)){
+    cam.move(camSpeed, 0, 0)
+  }
+  // d
+  if(keyIsDown(65)){
+    cam.move(-camSpeed, 0, 0)
+  }
+  // w
+  if(keyIsDown(87)){
+    cam.move(0, -camSpeed, 0)
+  }
+  // s
+  if(keyIsDown(83)){
+    cam.move(0, camSpeed, 0)
+  }
+  // r
+  if(keyIsDown(82)){
+    cam.move(0, 0, camSpeed)
+  }
+  if(keyIsDown(70)){
+    cam.move(0, 0, -camSpeed)
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+let boxLength = 150 
+let boxWidth = 25
+let ajustment = boxLength/2 - boxWidth/2
+let halfAjustment = boxLength/4 - boxWidth/2
+
+let cam;
+let camX = 800;
+let camY = 0;
+let camZ = 0;
+
+let camSpeed = 5
+let camRotation = 0.002
+
+let triangles = []
+
+let previousX = -1
+let previousY = -1
+let pan = 0
+let tilt = 0
+
+class Penrose{
+  constructor(x, y, z){
+    this.x = 0
+    this.y = 0
+    this.z = -1000
+    this.pan = pan
+    this.tilt = tilt
+    this.camX = cam.eyeX
+    this.camY = cam.eyeY
+    this.camZ = cam.eyeZ
+  }
+  
+  drawPenrose(){
+    push()
+    translate(this.camX, this.camY, this.camZ)
+    rotateY(this.tilt)
+    rotateX(this.pan)
+    translate(this.x, this.y, this.z)
+    rotateX(54.736 * (PI/180))
+    rotateZ(45 * (PI/180))
+    // Draw the top box
+    box(boxLength, boxWidth, boxWidth)
+    push()
+      translate(ajustment, ajustment, 0)
+      push()
+        rotateZ(90 * (PI/180))
+        // Draw the bottom box 
+        box(boxLength, boxWidth, boxWidth)
+      pop()
+      // Draw the first left box, coming up from the bottom box
+      // Use the clip to remove it
+      clip(function(){
+        translate(-ajustment, -ajustment, 0)
+        translate(-ajustment, 0, -halfAjustment)
+        rotateY(90 * (PI/180))
+        box(boxLength/2 * 0.9, boxWidth, boxWidth)
+        translate(ajustment, ajustment, 0)
+      }, {invert:true})
+      translate(0, ajustment, boxLength/3.8 - boxWidth/2)
+      rotateY(90 * (PI/180))
+      box(boxLength/1.9, boxWidth, boxWidth)
+    pop()
+    // Draw the second left box, coming down from the top box
+    push()
+      translate(-ajustment, 0, -halfAjustment)
+      rotateY(90 * (PI/180))
+      box(boxLength/2, boxWidth, boxWidth)
+    pop()
+    pop()
+  }
+}
+function mouseDragged(){
+  // Ignore the mouse movement of each camera
+  if(previousX === -1 && previousY === -1){
+    previousX = mouseX
+    previousY = mouseY
+    return
+  }
+  let xDiff = mouseX-previousX
+  let yDiff = previousY-mouseY
+      
+  tilt += xDiff * camRotation
+  pan += yDiff * camRotation
+  
+  // Equasions determined by chatGPT, implementation by me
+  let newX = -800 * cos(pan) * sin(tilt)
+  let newY = 800 * sin(pan)
+  let newZ = -800 * cos(pan) * cos(tilt)
+  
+  newX += cam.eyeX
+  newY += cam.eyeY
+  newZ += cam.eyeZ
+  
+  cam.lookAt(newX, newY, newZ)
+  
+  previousX = mouseX
+  previousY = mouseY
+}
+  
+function mouseReleased(){
+  previousX = -1
+  previousY = -1
+}
+
+function keyPressed(){
+  if(key == "e"){
+    triangles.push(new Penrose(cam.centerX, cam.centerY, cam.centerZ))
+  }
 }
