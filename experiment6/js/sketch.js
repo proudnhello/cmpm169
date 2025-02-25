9461cee3e47133de36007cecbcb437e0434bd1c8
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-02-24 17:34:14 -0800

    job done

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,11 +1,77 @@
 let canvasContainer = $("#canvas-container");
 
+let latContainer = $("#lat");
+let longContainer = $("#long");
+let radiusContainer = $("#radius");
+let searchButton = $("#search-button");
+
+searchButton.click(function() {
+    let lat = latContainer.val();
+    let long = longContainer.val();
+    let radius = radiusContainer.val();
+
+    if(apiKey === "null") {
+        apiKey = prompt("Please enter your Google API key");
+        if(apiKey === null || apiKey === "" || apiKey === "null") {
+            alert("Please enter a valid API key to continue");
+            apiKey = "null";
+            return;
+        }
+    }
+
+    let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&order=viewCount&key='+apiKey+'&location='+lat+','+long+'&locationRadius='+radius+'mi';
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(handleSearchResults);
+});
+
 // Fetch the json data
-let data = fetch('./resources/harrison.json')
+let initialData = fetch('./resources/harrison.json')
   .then(response => response.json())
   .then(processData);
+
+let apiKey = "null"
+
+function handleSearchResults(data) {
+    if(data['error'] !== undefined) {
+        console.log(data);
+        alert("An error occurred: " + data['error']['message']);
+        // If the error is due to an invalid API key, prompt the user to enter a new one
+        console.log(data['error']['details']['0']['reason']);
+        if(data['error']['code'] === 403 || data['error']['details']['0']['reason'] === 'API_KEY_INVALID') {
+            apiKey = "null"
+        }
+        return;
+    }
+    let videoIds = [];
+    for (let i = 0; i < data['items'].length; i++) {
+        videoIds.push(data['items'][i]['id']['videoId']);
+    }
+
+    let videoIdString = videoIds.join(',');
+
+    let url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id='+videoIdString+'&key='+apiKey;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(processData);
+}
   
 function processData(data) {
+    if(data['error'] !== undefined) {
+        console.log(data);
+        alert("An error occurred: " + data['error']['message']);
+        // If the error is due to an invalid API key, prompt the user to enter a new one
+        console.log(data['error']['details']['0']['reason']);
+        if(data['error']['code'] === 403 || data['error']['details']['0']['reason'] === 'API_KEY_INVALID') {
+            apiKey = "null"
+        }
+        return;
+    }
+
+    canvasContainer.empty();
+
     let videoContainer = document.createElement('div');
     videoContainer.classList.add('video-container');
     canvasContainer.append(videoContainer);
